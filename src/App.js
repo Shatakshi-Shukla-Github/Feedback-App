62477f48ca0151d7bcd72767646a600237346187
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-01-31 23:53:47 +0530

    Added the Deafult Prop for styling the Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from "./components/Header"; 
 function App() {
-  return <div>
-    <Header/>
-  </div>;
+  return(
+  <>
+  <Header/> 
+  <div></div>
+  </>
+  );
 }
  
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-function Header() {
+import PropTypes from "prop-types";
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
   return (
-    <header>
+    <header style={headerStyles}>
       <div className="container">
-        <h1>Feedback App</h1>
+        <h1>{text}</h1>
       </div>
     </header>
   );
 }
 
+Header.defaultProps = {
+  text: "Feedback App",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
+Header.propTypes = {
+  text: PropTypes.string,
+};
 export default Header;
