cf036beea600e8a4a20b4df93d6f48d1073c253d
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-02-03 22:57:40 +0530

    Added conditional styling to the Card Component

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,5 +1,15 @@
-function Card({ children }) {
-  return <div className="card">{children}</div>;
+function Card({ children, reverse }) {
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: reverse ? "rgba(0,0,0,0.4)" : "#fff",
+        color: reverse ? "#fff" : "#000",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;
