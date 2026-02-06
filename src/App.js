ebd84a910483fb36c98392815044df50d2fd4c76
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-02-06 22:14:15 +0530

    Added the Total Reviews Functionality

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
    <>
     <Header/>
     <div className="container">
-      <FeedbackStats/>
+      <FeedbackStats feedback={feedback}/>
       <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
     </>
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,5 +1,9 @@
-function FeedbackStats() {
-  return <div>Stats</div>;
+function FeedbackStats({ feedback }) {
+  return (
+    <div className="feedback_stats">
+      <h4>{feedback.length} Reviews</h4>
+    </div>
+  );
 }
 
 export default FeedbackStats;
