8b54bc17004f996f47e8e00aa5926fb22a969932
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-02-07 19:55:56 +0530

    Added the Average feature using isFixed() method

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,15 @@
 function FeedbackStats({ feedback }) {
+  let average = Math.floor(
+    feedback.reduce((acc, cur) => acc + cur.rating, 0) / feedback.length,
+  );
+  // const decimal = (num) => {
+  //   return Number.parseFloat(num).toFixed(1);
+  // };
+  average = average.toFixed(1); //It will display average with only 1 digit after the decimal
   return (
-    <div className="feedback_stats">
+    <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating:- {isNaN(average) ? 0 : average}/10</h4>
     </div>
   );
 }
