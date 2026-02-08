c8670bdcb5ed4e17f5eb09395d3b50bc953880ee
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-02-08 20:59:38 +0530

    Added the Validation Check for the input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,19 @@ import Card from "./shared/Card";
 import Button from "./shared/Button";
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Message must atleast have 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -17,10 +29,11 @@ function FeedbackForm() {
             onChange={handleTextChange}
             value={text}
           />
-          <Button type="submit" version="primary">
+          <Button type="submit" version="primary" isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
