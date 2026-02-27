16ad7451467648a47e011e761fd79e812b2ffe06
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-02-28 00:34:58 +0530

    Added the fully functional edit functionality

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,8 @@ import RatingSelect from "./RatingSelect";
 import { useContext } from "react";
 import FeedbackContext from "./context/FeedbackContext";
 function FeedbackForm() {
-  const { addFeedback, feedbackEdit } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
   useEffect(() => {
     if (feedbackEdit.edit === true) {
       setBtnDisabled(false);
@@ -37,7 +38,11 @@ function FeedbackForm() {
         text: text,
         rating: rating,
       };
-      addFeedback(newFeedback);
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
+      } else {
+        addFeedback(newFeedback);
+      }
       setText("");
     }
   };
