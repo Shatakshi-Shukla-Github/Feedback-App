e01130f6b29f07712eb09357e36cfe6797afd4fe
Shatakshi Shukla <shatakshishukla49@gmail.com>
2026-03-09 19:55:47 +0530

    Added Update Functionality using JSON server

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -43,9 +43,17 @@ export const FeedbackProvider = ({ children }) => {
       edit: true,
     });
   };
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)),
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item)),
     );
   };
   return (
